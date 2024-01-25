b8f6c1df53418669b89613bdbd32f58b0c9fdbef
danggeuni <dydrnxkeh@naver.com>
2024-01-25 18:24:02 +0900

    1

--- springboot-developer/src/main/java/me/junhojung/springbootdeveloper/controller/BlogViewController.java
+++ springboot-developer/src/main/java/me/junhojung/springbootdeveloper/controller/BlogViewController.java
@@ -28,24 +28,4 @@ public class BlogViewController {
 
         return "articleList";
     }
-
-    @GetMapping("/articles/{id}")
-    public String getArticle(@PathVariable Long id, Model model){
-        Article article = blogService.finById(id);
-        model.addAttribute("article", new ArticleViewResponse(article));
-
-        return "article";
-    }
-
-    @GetMapping("/new-article")
-    public String newArticle(@RequestParam(required = false) Long id, Model model){
-        if(id == null){
-            model.addAttribute("article", new ArticleViewResponse());
-        }else {
-            Article article = blogService.finById(id);
-            model.addAttribute("article", new ArticleViewResponse(article));
-        }
-
-        return "newArticle";
-    }
 }
